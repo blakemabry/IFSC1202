c6fcdcab46d0e8990f30c143323884418d1d1c94
blakemabry <111765066+blakemabry@users.noreply.github.com>
2023-09-20 20:39:29 +0000

    0.4

--- 0.4 Project.py
+++ 0.4 Project.py
@@ -1,8 +1,11 @@
 x=int(input("Enter Start of Range:"))
 y=int(input("Enter End of Range:"))
 print("Prime Numbers Between", x, "and", y)
-for num in range(x, y + 1):
-    n=(num / 2) + 1
-    for i in range(2,num):
-        if n % num == 0:
-
+for i in range(x, y + 1):
+    num=0
+    for j in range(2,(i//2 +1)):
+        if (i%j==0):
+            num=1
+            break
+    if (num==0):
+        print(i)
