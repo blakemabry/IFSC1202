38e343297ab25125653c6ff15d82cde230f4550d
blakemabry <111765066+blakemabry@users.noreply.github.com>
2023-10-09 14:05:41 +0000

    0.6

--- 06.Project.py
+++ 06.Project.py
@@ -1,1 +1,27 @@
-x= open("06. Project input file.txt")
+inputfile = open("06. Project input file.txt" , "r")
+outputfile = open("06.Project Output File.txt" , "w")
+inputrecord = 0
+mergerecord = 0
+outputrecord = 0
+line = inputfile.readline()
+while line != "":
+    if line == "Insert Merge File Here**\n":
+        mergefile = open("06.Project Merge File.txt", "r")
+        merge = mergefile.readline()
+        while merge !="":
+            outputfile.write(merge)
+            outputrecord +=1
+            merge = mergefile.readline()
+            mergerecord += 1
+        outputfile.write("\n")
+        mergefile.close()
+    else:
+        outputfile.write(line)
+        outputrecord +=1
+        inputrecord +=1
+        line = inputfile.readline()
+inputfile.close()
+outputfile.close()
+print(inputrecord, "Input File Record")
+print(mergerecord, "Merge File Record")
+print(outputrecord, "Output File record")
